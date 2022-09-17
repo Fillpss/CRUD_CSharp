b6a40cf04b0bb636bfaa6b23d90c04decc49c4a1
Filipe Pinho <82127494+Fillpss@users.noreply.github.com>
2022-09-16 21:55:26 -0300

    Add files via upload

---
+++ EFGetStarted/Migrations/20220916235024_SecondCriate.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFGetStarted.Migrations
+{
+    public partial class SecondCriate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Data",
+                table: "Posts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Data",
+                table: "Posts");
+        }
+    }
+}
---
+++ EFGetStarted/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+using var database = new BloggingContext();
+
+Console.WriteLine($"Database path: {database.DbPath}");
+
+// Create 
+Console.WriteLine("Inserting a new blog");
+database.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+database.SaveChanges();
+
+// Read
+Console.WriteLine("Querying for a blog");
+var blog = database.Blogs.OrderBy(b => b.BlogId).FirstOrDefault();
+
+// Update
+Console.WriteLine("Updating the blog and adding a post");
+blog.Url = "https://devblogs.microsoft.com/dotnet";
+blog.Posts.Add(new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+database.SaveChanges();
+
+// Delete
+Console.WriteLine("Delete the blog");
+database.Remove(blog);
+
+
+// Atualizar os dados do banco
+database.SaveChanges();
